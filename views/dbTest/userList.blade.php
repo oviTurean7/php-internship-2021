eca483398cd229eb489e62d7ce53b008c4889b8f
robertpg3 <robertpgherghel@gmail.com>
2021-08-20 15:48:44 +0300

    user list style

--- views/dbTest/userList.blade.php
+++ views/dbTest/userList.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.main')
 
 @section('content')
-    <ul>
+    <ul class="user-list">
         @foreach($users as $user)
             <li>
-                <div>
-                    <p>{{$user['id']}}</p>
-                    <p>{{$user['first_name']}}</p>
-                    <p>{{$user['last_name']}}</p>
+                <div class="user-details">
+                    <p class="user-info">ID: <span>{{$user['id']}}</span></p>
+                    <p class="user-info">name: <span>{{$user['first_name']}} {{$user['last_name']}}</span></p>
+                    <p class="user-info">email: <span>{{$user['email']}}</span></p>
                 </div>
             </li>
         @endforeach
