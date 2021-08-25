8360f84a767e51bcd55ae020deeb714f674c8e5a
robertpg3 <robertpgherghel@gmail.com>
2021-08-25 14:48:44 +0300

    magic methods

--- public/oop/Training.php
+++ public/oop/Training.php
@@ -1,1 +1,53 @@
 <?php
+
+class Training
+{
+    /**  Location for overloaded data.  */
+    private $data = array();
+
+    /**  Overloading not used on declared properties.  */
+    public $declared = 1;
+
+    /**  Overloading only used on this when accessed outside the class.  */
+    private $hidden = 2;
+
+    public function __set($name, $value)
+    {
+        echo "Setting '$name' to '$value'\n";
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        echo "Getting '$name'\n";
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined property via __get(): ' . $name .
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
+            E_USER_NOTICE);
+        return null;
+    }
+
+    public function __isset($name)
+    {
+        echo "Is '$name' set?\n";
+        return isset($this->data[$name]);
+    }
+
+    public function __unset($name)
+    {
+        echo "Unsetting '$name'\n";
+        unset($this->data[$name]);
+    }
+
+    /**  Not a magic method, just here for example.  */
+    public function getHidden()
+    {
+        return $this->hidden;
+    }
+}
